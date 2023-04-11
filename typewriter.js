1791510218c0179c34f4fbb511eb21ad21e2f936
DanRoss88 <danielmartinross@gmail.com>
2023-04-11 15:01:29 -0700

    typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,8 @@
 const sentence = "hello there from lighthouse labs \n"
 let interval = 50;
 for (const char of sentence) {
-
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, interval);
-  interval += 50;
+setTimeout(() => 
+process.stdout.write(char), interval
+);
+interval += 50;
 };
