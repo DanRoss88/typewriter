b28a55bbf7467f64c89f77c5ede9c3cee3f67942
DanRoss88 <danielmartinross@gmail.com>
2023-04-11 13:58:56 -0700

    timeout typewriter

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs \n"
+let interval = 50;
+for (const char of sentence) {
+
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, interval);
+  interval += 50;
+};
